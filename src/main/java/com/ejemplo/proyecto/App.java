36e89a7599dfe9adc2aa23e43eac2001920eec45
Johan Heimar Prieto Benavides <jheimar@gmail.com>
2024-04-18 11:45:05 -0500

    first commit

---
+++ src/main/java/com/ejemplo/proyecto/App.java
@@ -0,0 +1,7 @@
+package com.tuempresa.proyecto;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo Maven!");
+    }
+}
---
+++ src/test/java/com/ejemplo/proyecto/AppTest.java
@@ -0,0 +1,11 @@
+package com.ejemplo.proyecto;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
