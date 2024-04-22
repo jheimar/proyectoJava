b77d5546a239a1bee3d982f9fb8801216491ce41
Johan Heimar Prieto Benavides <jheimar@gmail.com>
2024-04-21 22:23:12 -0500

    Codigo suma

--- src/main/java/com/ejemplo/proyecto/App.java
+++ src/main/java/com/ejemplo/proyecto/App.java
@@ -2,6 +2,10 @@ package com.ejemplo.proyecto;
 
 public class App {
     public static void main(String[] args) {
+        int suma = 0;
+        int valor1 = 2;
+        int valor2 = 5;
+        suma = valor1 + valor2;
         System.out.println("Hola Mundo Maven!");
     }
 }
